e9fc738d110d2b2e59c34fec696c67c001dbe788
Edvinas <edvinasalimas98@gmail.com>
2022-10-08 22:33:55 +0300

    SHOP-109 Add new component

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,40 @@
+import React from 'react';
+import { animated, config, Transition } from 'react-spring';
 import './App.scss';
+import MyComponent from './components/MyComponent/MyComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <div className="App-header-box">
-          <div>Edvinas Alimas</div>
-          <div className="App-contact-button-wrapper">
-            <div className="wrapper">
-              <a href="#" className="App-button"><span>Contact</span></a>
+class App extends React.Component {
+
+  state = {
+    show: false
+  }
+
+  setStateShow() {
+    // eslint-disable-next-line react/no-direct-mutation-state
+    this.state.show = !this.state.show;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="App-header-box">
+            <div><MyComponent></MyComponent></div>
+            <div className="App-contact-button-wrapper">
+              <div className="wrapper">
+                <a href="#" className="App-button"><span>Contact</span></a>
+              </div>
             </div>
           </div>
-        </div>
-      </header>
-      <body className="App-body">
-        <div>
-
-        </div>
-      </body>
-    </div>
-  );
+        </header>
+        <body className="App-body">
+          <div>
+        
+          </div>
+        </body>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ app/src/components/MyComponent/MyComponent.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { Transition, config, animated } from 'react-spring';
+import './MyComponent.scss';
+
+class MyComponent extends React.Component {
+
+  state = {
+    show: true
+  }
+
+  setStateShow() {
+    // eslint-disable-next-line react/no-direct-mutation-state
+    this.state.show = !this.state.show;
+  }
+
+  render() {
+    return (
+      <Transition
+        items={this.state.show}
+        from={{ opacity: 0 }}
+        enter={{ opacity: 1 }}
+        reverse={this.state.show}
+        delay={200}
+        config={config.molasses}
+        onRest={() => this.setStateShow()}>
+        {(styles, item) =>
+          item && <animated.div style={styles}>
+            Edvinas Alimas
+            </animated.div>
+        }
+      </Transition>
+    )
+  }
+
+}
+export default MyComponent;
