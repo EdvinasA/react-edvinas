9456401e140ba9c142a867113c082af58617bdf2
Edvinas <edvinasalimas98@gmail.com>
2022-12-29 21:46:35 +0200

    Add contact

--- app/src/components/ContactComponent/ContactComponent.tsx
+++ app/src/components/ContactComponent/ContactComponent.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './ContactComponent.scss';
 import {TextField} from "@mui/material";
-
-interface ContactComponentProps {
-}
+import {send} from "emailjs-com";
 
 const ContactComponent = () => {
   const [contact, setContact] = React.useState({
@@ -11,6 +9,33 @@ const ContactComponent = () => {
     email: '',
     message: ''
   });
+
+  const onSubmit = () => {
+    console.log(contact);
+    send(
+        'service_crp7x0f',
+        'template_z2ptpyq',
+        {
+          message: `name: ${contact.name}, email: ${contact.email}, message: ${contact.message}`,
+        },
+        'BdWj8gR1-lWr9yNsx'
+    )
+    .then((response) => {
+      console.log('SUCCESS!', response.status, response.text);
+    })
+    .catch((err) => {
+      console.log('FAILED...', err);
+    });
+    setContact({...contact, message: ''});
+    setContact({...contact, name: ''});
+    setContact({...contact, email: ''});
+  };
+
+  const handleChange = (e: any) => {
+    setContact({ ...contact, [e.target.name]: e.target.value });
+  };
+
+
   return (
       <div className="contact-wrapper">
         <div className='contact-title'>
@@ -18,7 +43,7 @@ const ContactComponent = () => {
         </div>
         <div>
           <div></div>
-          <div className='contact-field'><TextField fullWidth sx={{
+          <div className='contact-field'><TextField name='name' onChange={handleChange} fullWidth sx={{
             '& .MuiInput-underline:before': { borderBottomColor: 'white' },
             '& .MuiInput-underline:after': { borderBottomColor: 'white' },
           }}
@@ -26,7 +51,7 @@ const ContactComponent = () => {
                                                     InputLabelProps={{
                                                       style: { color: 'white' },
                                                     }}/></div>
-          <div className='contact-field'><TextField fullWidth sx={{
+          <div className='contact-field'><TextField name='email' onChange={handleChange} fullWidth sx={{
             '& .MuiInput-underline:before': { borderBottomColor: 'white' },
             '& .MuiInput-underline:after': { borderBottomColor: 'white' },
           }}
@@ -34,7 +59,7 @@ const ContactComponent = () => {
                                                     InputLabelProps={{
                                                       style: { color: 'white' },
                                                     }}/></div>
-          <div className='contact-field'><TextField fullWidth multiline rows={4} sx={{
+          <div className='contact-field'><TextField name='message' onChange={handleChange} fullWidth multiline rows={4} sx={{
             '& .MuiInput-underline:before': { borderBottomColor: 'white' },
             '& .MuiInput-underline:after': { borderBottomColor: 'white' },
           }}
@@ -42,7 +67,7 @@ const ContactComponent = () => {
                                                     InputLabelProps={{
                                                       style: { color: 'white' },
                                                     }}/></div>
-          <div className='submit-button-wrapper'><button className='submit-button'>Submit</button></div>
+          <div className='submit-button-wrapper'><button className='submit-button' onClick={onSubmit}>Submit</button></div>
         </div>
       </div>
   );
